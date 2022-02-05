fb457198e234ecf882fa27c3b35df94a752f0c9f
Catalin Tugui <katamaxx23@gmail.com>
2022-02-05 14:10:20 +0200

    starter files

---
+++ blog-client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>To be written smth</h1>
+    </div>
+  );
+}
+
+export default App;
