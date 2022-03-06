b80a2856ca595c918ad89e30167495792b991652
Catalin Tugui <katamaxx23@gmail.com>
2022-03-06 22:36:31 +0200

    added loading div

--- blog-client/src/Home.js
+++ blog-client/src/Home.js
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react"
 import ArticlesList from './ArticlesList'
 
 const Home = () => {
-    const [articles, setArticles] = useState(null)
+    const [articles, setArticles] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
 
     const handleDelete = (id) => {
         const updatedArticles = articles.filter(item => item.id !== id);
@@ -11,13 +12,19 @@ const Home = () => {
 
     useEffect(() => {
         fetch('http://localhost:8800/articles')
-        .then(response => response.json())
-        .then(data => setArticles(data))
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            setArticles(data);
+            setIsLoading(false);
+        })
     }, [])
 
     return (
         <div className="home">
-           {articles && <ArticlesList articles={articles} handleDelete={handleDelete}/>}
+            {isLoading && <div>Loading...</div>}
+            {articles && <ArticlesList articles={articles} handleDelete={handleDelete}/>}
         </div>
     )
 }
