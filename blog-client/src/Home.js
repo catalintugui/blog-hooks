8a1259c2590dcbaa26c4955c42274607a15a57f3
Catalin Tugui <katamaxx23@gmail.com>
2022-03-07 23:46:10 +0200

    error handler

--- blog-client/src/Home.js
+++ blog-client/src/Home.js
@@ -4,6 +4,7 @@ import ArticlesList from './ArticlesList'
 const Home = () => {
     const [articles, setArticles] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     const handleDelete = (id) => {
         const updatedArticles = articles.filter(item => item.id !== id);
@@ -11,18 +12,29 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch('http://localhost:8800/articles')
-        .then(response => {
-            return response.json()
-        })
-        .then(data => {
-            setArticles(data);
-            setIsLoading(false);
-        })
-    }, [])
+        setTimeout(() => {
+            fetch('http://localhost:8800/articles')
+            .then(response => {
+                if(!response.ok){
+                    throw Error('could not fetch the data')
+                }
+                return response.json()
+            })
+            .then(data => {
+                setArticles(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch(error => {
+                setIsLoading(false);
+                setError(error.message);
+            })
+        }, 1000)
+    }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {articles && <ArticlesList articles={articles} handleDelete={handleDelete}/>}
         </div>
